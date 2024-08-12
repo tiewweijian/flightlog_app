3b03502843d9d1fd05ded2984b65f84e958571eb
tiewweijian <tiewweijian@u.nus.edu>
2024-08-13 00:25:17 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/flightlog_app/precache-manifest.f1a5cd06a4df6bca200729ad9ea01d14.js"
+  "/flightlog_app/precache-manifest.d9685464b044f6ec4ab3a541001c5034.js"
 );
 
 self.addEventListener('message', (event) => {
